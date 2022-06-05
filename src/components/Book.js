ab99227451ff95b771ba9baf4a22b24b639de2b0
Minh Nguyens <minhnguyen@wee.vn>
2022-06-06 03:29:47 +0700

    update add, delete and view

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,21 +3,20 @@ import { FaTimes } from "react-icons/fa";
 const Book = ({ book, onDelete }) => {
   return (
     <div className="book">
-      <h4>
+      <h3>
         {" "}
         Name: {book.name}{" "}
         <FaTimes
           style={{ color: "red", cursor: "pointer" }}
           onClick={() => onDelete(book.id)}
         />{" "}
-      </h4>
-      <p>author: {book.author}</p>
-
-      <p> Publication Year: {book.pubYear} </p>
+      </h3>
+      <p>Author: {book.author}</p>
 
       <p> Rating: {book.rating} </p>
+      <p> ISBN: {book.isbn}</p>
 
-      {book.isbn ? <p> ISBN: {book.isbn} </p> : ""}
+      {/* {book.isbn ? <p> ISBN: {book.isbn} </p> : ""} */}
     </div>
   );
 };
